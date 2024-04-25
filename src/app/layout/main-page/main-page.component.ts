4b42eaeed605211cb0b5868f5a4a0d37c55abdd5
ritsudo <minato-namikaje@yandex.ru>
2024-04-25 13:44:03 +0300

    feat: add search to main page

--- src/app/layout/main-page/main-page.component.ts
+++ src/app/layout/main-page/main-page.component.ts
@@ -4,16 +4,21 @@ import { CommonModule } from '@angular/common';
 import { Item } from '../../dto/item.model';
 import { Observable } from 'rxjs';
 import { GetAllResult } from '../../dto/getAllResult.model';
+import { backendUrl } from '../../app.config';
+import { categories } from '../../dto/categories';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
 
 export class MainPageComponent {
+  category = categories;
+  apiUrl = backendUrl;
 
   constructor(
     private itemService: ItemService
@@ -21,6 +26,23 @@ export class MainPageComponent {
 
   result!: Observable<GetAllResult>;
   items: Item[] = [];
+  updatedItems: Item[] = [];
+
+  searchTerm: string = '';
+
+  updateValues(): void {
+    
+    this.itemService.getItemsByName(this.searchTerm).subscribe(response => {
+      this.result = response;
+      this.items.splice(0, this.items.length);
+      this.updatedItems = response;
+      console.log(this.updatedItems);
+      for (let i = 0; i < this.updatedItems.length; i+=1) {
+        this.items.push(new Item(this.updatedItems[i].id, this.updatedItems[i].name, this.updatedItems[i].subCategoryId, this.updatedItems[i].location, this.updatedItems[i].price, this.updatedItems[i].mainImageId
+        ));
+      }
+    });
+  }
 
   ngOnInit(): void {
     this.itemService.getItems().subscribe(response => {
--- src/app/services/item/item.service.ts
+++ src/app/services/item/item.service.ts
@@ -1,16 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { GetAllResult } from '../../dto/getAllResult.model';
+import { backendUrl } from '../../app.config';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ItemService {
-  private apiUrl = 'https://localhost:44315'; // Replace with your API endpoint URL
+  private apiUrl = backendUrl;
 
   constructor(
     private http: HttpClient
   ) { }
+
+  getItemsByName(name: string) {
+    return this.http.get<any>(`${this.apiUrl}/Item/by-name?Name=${name}`);
+  }
 
   getItems() {
     return this.http.get<any>(`${this.apiUrl}/Item/all`);
